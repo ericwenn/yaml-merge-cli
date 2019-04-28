f0f835f93412608fe068798a8b18e32dd04e2a51
Eric Wennerberg <ericwenn@gmail.com>
2019-04-28 12:56:49 +0200

    Initial merge cmd

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/ericwenn/yaml-merge-cli
+
+go 1.12
+
+require (
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
+	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/imdario/mergo v0.3.7
+	github.com/stretchr/testify v1.3.0 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	gopkg.in/yaml.v2 v2.2.2
+)
---
+++ merge.go
@@ -0,0 +1,36 @@
+package main // import "github.com/ericwenn/yaml-merge-cli"
+
+import (
+	"fmt"
+	"github.com/imdario/mergo"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	app := kingpin.New("yaml-merge", "merge yaml files using mergo")
+	files := app.Arg("files", "Files to merge, from left to right").Required().ExistingFiles()
+	_ = kingpin.MustParse(app.Parse(os.Args[1:]))
+
+	merged := make(map[interface{}]interface{})
+	for i, file := range *files {
+		b, err := ioutil.ReadFile(file)
+		if err != nil {
+			app.Fatalf("unable to read file %d: %v", i, err)
+		}
+		m := make(map[interface{}]interface{})
+		if err := yaml.Unmarshal(b, &m); err != nil {
+			app.Fatalf("unable to unmarshal file %d: %v", i, err)
+		}
+		if err := mergo.Merge(&merged, m, mergo.WithOverride, mergo.WithAppendSlice); err != nil {
+			app.Fatalf("unable to merge file %d: %v", i, err)
+		}
+	}
+	out, err := yaml.Marshal(merged)
+	if err != nil {
+		app.Fatalf("unable to marshall output: %v", err)
+	}
+	fmt.Print(string(out))
+}
